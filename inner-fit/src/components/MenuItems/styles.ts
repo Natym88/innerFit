62897eea4a18f8b83b476b28931a69975a98c4a3
Natalia Moreira <Natalia.Moreira@cmsenergy.com>
2023-11-22 17:31:43 -0300

    probando deploy vercel

--- inner-fit/src/components/MenuItems/styles.ts
+++ inner-fit/src/components/MenuItems/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 interface IStyledList {
     mobile: boolean;
 }
+// voy a probar deploy
 
 export const StyledList = styled.ul<IStyledList>`
     display: flex;
